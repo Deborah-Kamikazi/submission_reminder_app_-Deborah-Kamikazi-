1acb8bfe60a72ebbb9d0ce9fb3dc215dbb365ef1
deborah <deborahkamikazi9@gmail>
2025-06-20 11:40:09 +0200

    removing submission after tatsing

--- submission_reminder_Deborah Kamikazi/scripts/functions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-list_pending_submissions() {
-  while IFS=',' read -r name status; do
-    if [[ "$status" == "pending" ]]; then
-      echo "$name needs to submit"
-    fi
-  done < "$(dirname "$0")/../data/submissions.txt"
-}
--- submission_reminder_Deborah Kamikazi/scripts/reminder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source "$(dirname "$0")/functions.sh"
-source "$(dirname "$0")/../config/config.env"
-
-echo "Checking for pending submissions for $ASSIGNMENT..."
-list_pending_submissions
